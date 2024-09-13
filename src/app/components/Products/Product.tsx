39cb042f2010aeb62ecddcb0ab0a7c621ad8fdf0
Rohan Maharjan <maharjanrohan16@gmail.com>
2024-09-13 09:47:42 +0545

    search

--- src/app/components/Products/Product.tsx
+++ src/app/components/Products/Product.tsx
@@ -3,7 +3,8 @@ import { api } from "@/service/service-axios";
 import { Box, Button, Flex, Grid, Input, Text } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
 import Loader from "../Loader";
 
 interface ItemProps {
@@ -14,6 +15,8 @@ interface ItemProps {
 }
 
 const Product: React.FC<ItemProps> = () => {
+  const [searchText, setSearchText] = useState("");
+  const [filterData, setFilterData] = useState<ItemProps[]>([]);
   const { data: apiData, isFetching } = useQuery({
     queryKey: ["product"],
     queryFn: api,
@@ -23,17 +26,40 @@ const Product: React.FC<ItemProps> = () => {
     return <Loader height="100%" />;
   }
 
+  const search = (searchText: string) => {
+    if (!apiData) return;
+
+    if (searchText === "") {
+      setFilterData(apiData);
+    } else {
+      const filteredData = apiData?.filter((item) =>
+        item.title.toLowerCase().includes(searchText.toLowerCase())
+      );
+      setFilterData(filteredData);
+    }
+  };
+
   return (
     <Box>
       <Flex my={2}>
-        <Input placeholder="Search Products" size="md" mr={5} />
-        <Button colorScheme="teal" variant="solid">
+        <Input
+          placeholder="Search Products"
+          size="md"
+          mr={5}
+          value={searchText}
+          onChange={(event) => setSearchText(event.target.value)}
+        />
+        <Button
+          onClick={() => search(searchText)}
+          colorScheme="teal"
+          variant="solid"
+        >
           Search
         </Button>
       </Flex>
       <Box>
         <Grid templateColumns={"repeat(3,1fr)"} gap={5}>
-          {apiData?.map((item) => (
+          {(filterData.length > 0 ? filterData : apiData)?.map((item) => (
             <Box border={"2px"} borderColor={"gray.200"} key={item.id} p={5}>
               <Image
                 style={{ padding: "20%" }}
@@ -48,6 +74,14 @@ const Product: React.FC<ItemProps> = () => {
               <Text textAlign={"center"} fontSize="2xl">
                 Rs:{item.price}
               </Text>
+              <Flex justifyContent={"space-around"}>
+                <Button>
+                  <Link href={"/cart"}>Add to Cart</Link>
+                </Button>
+                <Link href={`/details/${item.id}`}>
+                  <Button>View Details</Button>
+                </Link>
+              </Flex>
             </Box>
           ))}
         </Grid>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,53 @@
-import React from "react";
+"use client";
+import { Flex, Input } from "@chakra-ui/react";
+// import { DevTool } from "@hookform/devtools";
+import { useForm } from "react-hook-form";
+
+type LoginProps = {
+  username: string;
+  password: string;
+};
 
 export default function Login() {
+  const {
+    register,
+    // control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginProps>();
+  const onSubmit = (data: LoginProps) => {
+    console.log("Form Submitted", data);
+  };
   return (
-    <div>
+    <Flex direction={"column"} alignItems={"center"} p={4}>
       <h1>Login</h1>
-    </div>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <Flex direction={"column"} gap={4} align={"center"}>
+          <Input
+            autoComplete="off"
+            placeholder="Enter UserName"
+            border="1px"
+            type="text"
+            id="username"
+            {...register("username", { required: "Username is required" })}
+          />
+          {errors.username && <p>Username is required</p>}
+
+          <Input
+            autoComplete="off"
+            placeholder="Enter password"
+            border="1px"
+            type="password"
+            id="password"
+            {...register("password", { required: "Password is required" })}
+            required
+          />
+          {errors.password && <p>Password is required</p>}
+
+          <Input type="submit" />
+        </Flex>
+      </form>
+      {/* <DevTool control={control} /> */}
+    </Flex>
   );
 }
